852cca1855ec0cff840562571862df89feb7e821
Will Evans <62522222+AuctortiA@users.noreply.github.com>
2021-05-30 20:45:49 +0100

    added check for pawn moves i.e. correct number of squares & direction moved

--- code/chess_model/pieces.py
+++ code/chess_model/pieces.py
@@ -26,28 +26,68 @@ class King (Piece):
         return -1 <= rank_dif <= 1 and -1 <= file_dif <= 1
         
 
-
 class Queen (Piece):
     def __init__(self, code, file, rank) -> None:
         super().__init__(code, file, rank)
+
+    def valid_move(self, old, new):
+        old_rank, old_file = old
+        new_rank, new_file = new
+
+        rank_dif = old_rank - new_rank
+        file_dif = old_file - new_file 
 
 
 class Rook (Piece):
     def __init__(self, code, file, rank) -> None:
         super().__init__(code, file, rank)
 
+    def valid_move(self, old, new):
+        old_rank, old_file = old
+        new_rank, new_file = new
+
+        rank_dif = old_rank - new_rank
+        file_dif = old_file - new_file 
+
 
 class Bishop (Piece): 
     def __init__(self, code, file, rank) -> None:
         super().__init__(code, file, rank)
+
+    def valid_move(self, old, new):
+        old_rank, old_file = old
+        new_rank, new_file = new
+
+        rank_dif = old_rank - new_rank
+        file_dif = old_file - new_file  
 
 
 class Knight (Piece):
     def __init__(self, code, file, rank) -> None:
         super().__init__(code, file, rank)
 
+    def valid_move(self, old, new):
+        old_rank, old_file = old
+        new_rank, new_file = new
+
+        rank_dif = old_rank - new_rank
+        file_dif = old_file - new_file 
 
 class Pawn (Piece):
     def __init__(self, code, file, rank) -> None:
         super().__init__(code, file, rank)
 
+    def valid_move(self, old, new):
+        old_rank, old_file = old
+        new_rank, new_file = new
+
+        rank_dif = old_rank - new_rank
+        file_dif = old_file - new_file 
+
+        if 0 <= rank_dif <= 2 and file_dif == 0:
+            return True
+        
+        if rank_dif == 1 and -1 <= file_dif <= 1:
+            return True
+
+        return False
